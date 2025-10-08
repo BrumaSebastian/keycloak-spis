59ed493d4e4dfc2034225faa8b6436a235465ad3
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-10-09 01:22:19 +0300

    try to use a imported realm for a test

--- keycloak-spis/spi-resources/src/test/java/com/extensions/spis/resources/admin/test/UsersAdminResourceIntegrationTest.java
+++ keycloak-spis/spi-resources/src/test/java/com/extensions/spis/resources/admin/test/UsersAdminResourceIntegrationTest.java
@@ -2,6 +2,8 @@ package com.extensions.spis.resources.admin.test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Objects;
 
@@ -13,28 +15,29 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.representations.AccessTokenResponse;
-import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.extensions.spis.resources.admin.factories.UsersExtensionAdminResourceFactory;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
-import groovy.util.logging.Slf4j;
 import io.quarkus.logging.Log;
 
 /**
  * Integration tests for UsersAdminResource using Testcontainers
  */
-@Slf4j
 @Testcontainers
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UsersAdminResourceIntegrationTest {
     @SuppressWarnings("resource")
     @Container
     private static final KeycloakContainer keycloak = new KeycloakContainer()
+            .withRealmImportFile("/main-realm.json")
             .withProviderClassesFrom("target/classes");
+
     private static TestDataManager testDataManager;
     private static String accessToken;
     private static final String TEST_REALM = "test-realm";
@@ -42,14 +45,15 @@ public class UsersAdminResourceIntegrationTest {
     @BeforeAll
     static void setUpAll() {
         Keycloak keycloakClient = keycloak.getKeycloakAdminClient();
-        testDataManager = new TestDataManager(keycloakClient);
+        // testDataManager = new TestDataManager(keycloakClient);
 
-        // Get access token
-        AccessTokenResponse tokenResponse = keycloakClient.tokenManager().getAccessToken();
-        accessToken = tokenResponse.getToken();
+        // // Get access token
+        // AccessTokenResponse tokenResponse =
+        // keycloakClient.tokenManager().getAccessToken();
+        // accessToken = tokenResponse.getToken();
 
-        // Create test realm
-        testDataManager.createTestRealm(TEST_REALM);
+        // // Create test realm
+        // testDataManager.createTestRealm(TEST_REALM);
     }
 
     @AfterAll
@@ -62,33 +66,47 @@ public class UsersAdminResourceIntegrationTest {
     @Test
     @Order(1)
     @DisplayName("Should get users from UsersAdminResource")
-    void shouldGetUsers() {
-        // Keycloak keycloakClient = keycloak.getKeycloakAdminClient();
-        // AccessTokenResponse accessTokenResponse =
-        // keycloakClient.tokenManager().getAccessToken();
-
-        // given().baseUri(keycloak.getAuthServerUrl())
-        // .basePath("/admin/realms/" + TEST_REALM + "/" +
-        // UsersExtensionAdminResourceFactory.ID)
-        // .auth().oauth2(accessToken)
-        // .when().get(")
-        // .then().statusCode(200)
-        // .body("size()", is(1));
+    void shouldRealmExist() {
+        assertTrue(keycloak.isRunning());
+        Keycloak keycloakClient = keycloak.getKeycloakAdminClient();
+        Log.info(keycloak.getAuthServerUrl());
+        RealmResource realm = keycloakClient.realm("main");
+        RealmRepresentation realmRepresentation = realm.toRepresentation();
+        assertNotNull(realmRepresentation);
     }
 
-    @Test
-    @Order(1)
-    @DisplayName("Should get a user from UsersAdminResource")
-    void shouldGetUser() {
-        String userId = testDataManager.createUser(TEST_REALM, TestProfiles.Users.simpleUser());
+    // @Test
+    // @Order(1)
+    // @DisplayName("Should get users from UsersAdminResource")
+    // void shouldGetUsers() {
+    // // Keycloak keycloakClient = keycloak.getKeycloakAdminClient();
+    // // AccessTokenResponse accessTokenResponse =
+    // // keycloakClient.tokenManager().getAccessToken();
 
-        Log.info(userId);
+    // // given().baseUri(keycloak.getAuthServerUrl())
+    // // .basePath("/admin/realms/" + TEST_REALM + "/" +
+    // // UsersExtensionAdminResourceFactory.ID)
+    // // .auth().oauth2(accessToken)
+    // // .when().get(")
+    // // .then().statusCode(200)
+    // // .body("size()", is(1));
+    // }
 
-        given().baseUri(keycloak.getAuthServerUrl())
-                .basePath("/admin/realms/" + TEST_REALM + "/" + UsersExtensionAdminResourceFactory.ID + "/")
-                .auth().oauth2(accessToken)
-                .when().get(userId)
-                .then().statusCode(200)
-                .body("id", is(userId));
-    }
+    // @Test
+    // @Order(2)
+    // @DisplayName("Should get a user from UsersAdminResource")
+    // void shouldGetUser() {
+    // String userId = testDataManager.createUser(TEST_REALM,
+    // TestProfiles.Users.simpleUser());
+
+    // Log.info(userId);
+
+    // given().baseUri(keycloak.getAuthServerUrl())
+    // .basePath("/admin/realms/" + TEST_REALM + "/" +
+    // UsersExtensionAdminResourceFactory.ID + "/")
+    // .auth().oauth2(accessToken)
+    // .when().get(userId)
+    // .then().statusCode(200)
+    // .body("id", is(userId));
+    // }
 }
