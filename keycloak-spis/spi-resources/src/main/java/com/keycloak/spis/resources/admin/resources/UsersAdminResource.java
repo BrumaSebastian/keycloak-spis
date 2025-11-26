b7e059b3816f15ec3f226b77e22edd160611c178
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-11-26 19:33:44 +0200

    improve the structure of default count users

--- keycloak-spis/spi-resources/src/main/java/com/keycloak/spis/resources/admin/resources/UsersAdminResource.java
+++ keycloak-spis/spi-resources/src/main/java/com/keycloak/spis/resources/admin/resources/UsersAdminResource.java
@@ -1,6 +1,7 @@
 package com.keycloak.spis.resources.admin.resources;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,14 @@ import java.util.Objects;
 import java.util.stream.Stream;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.resteasy.annotations.cache.NoCache;
+import org.keycloak.common.Profile;
 import org.keycloak.models.Constants;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
@@ -22,9 +28,10 @@ import org.keycloak.services.resources.KeycloakOpenAPI;
 import org.keycloak.services.resources.admin.AdminEventBuilder;
 import org.keycloak.services.resources.admin.fgap.AdminPermissionEvaluator;
 import org.keycloak.services.resources.admin.fgap.UserPermissionEvaluator;
+import org.keycloak.utils.SearchQueryUtils;
 
-import com.keycloak.spis.common.GroupRealmRoles;
 import com.keycloak.spis.common.PlatformRealmRoles;
+import com.keycloak.spis.common.models.CountRepresentation;
 import com.keycloak.spis.common.models.ExtendedUserRepresentation;
 import com.keycloak.spis.common.utils.EnumUtils;
 import com.keycloak.spis.common.utils.ModelToRepresentation;
@@ -43,6 +50,7 @@ public class UsersAdminResource {
     private final AdminEventBuilder adminEvent;
     private final KeycloakSession session;
     private final RealmModel realm;
+    private static final String SEARCH_ID_PARAMETER = "id:";
 
     public UsersAdminResource(KeycloakSession session, RealmModel realm, AdminPermissionEvaluator auth,
             AdminEventBuilder adminEvent) {
@@ -135,5 +143,120 @@ public class UsersAdminResource {
 
             return representation;
         });
+    }
+
+    @Path("count")
+    @GET
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = Integer.class))),
+            @APIResponse(responseCode = "403", description = "Forbidden")
+    })
+    @Tag(name = KeycloakOpenAPI.Admin.Tags.USERS)
+    @Operation(summary = "Returns the number of users that match the given criteria.", description = "It can be called in three different ways. "
+            +
+            "1. Don’t specify any criteria and pass {@code null}. The number of all users within that realm will be returned. <p> "
+            +
+            "2. If {@code search} is specified other criteria such as {@code last} will be ignored even though you set them. The {@code search} string will be matched against the first and last name, the username and the email of a user. <p> "
+            +
+            "3. If {@code search} is unspecified but any of {@code last}, {@code first}, {@code email} or {@code username} those criteria are matched against their respective fields on a user entity. Combined with a logical and.")
+    public CountRepresentation getUsersCount(
+            @Parameter(description = "A String contained in username, first or last name, or email. Default search behavior is prefix-based (e.g., foo or foo*). Use *foo* for infix search and \"foo\" for exact search.") @QueryParam("search") String search,
+            @Parameter(description = "A String contained in lastName, or the complete lastName, if param \"exact\" is true") @QueryParam("lastName") String last,
+            @Parameter(description = "A String contained in firstName, or the complete firstName, if param \"exact\" is true") @QueryParam("firstName") String first,
+            @Parameter(description = "A String contained in email, or the complete email, if param \"exact\" is true") @QueryParam("email") String email,
+            @Parameter(description = "A String contained in username, or the complete username, if param \"exact\" is true") @QueryParam("username") String username,
+            @Parameter(description = "whether the email has been verified") @QueryParam("emailVerified") Boolean emailVerified,
+            @Parameter(description = "The alias of an Identity Provider linked to the user") @QueryParam("idpAlias") String idpAlias,
+            @Parameter(description = "The userId at an Identity Provider linked to the user") @QueryParam("idpUserId") String idpUserId,
+            @Parameter(description = "Boolean representing if user is enabled or not") @QueryParam("enabled") Boolean enabled,
+            @Parameter(description = "Boolean which defines whether the params \"last\", \"first\", \"email\" and \"username\" must match exactly") @QueryParam("exact") Boolean exact,
+            @Parameter(description = "A query to search for custom attributes, in the format 'key1:value2 key2:value2'") @QueryParam("q") String searchQuery) {
+        UserPermissionEvaluator userPermissionEvaluator = auth.users();
+        userPermissionEvaluator.requireQuery();
+
+        Map<String, String> searchAttributes = searchQuery == null
+                ? Collections.emptyMap()
+                : SearchQueryUtils.getFields(searchQuery);
+        if (search != null) {
+            if (search.startsWith(SEARCH_ID_PARAMETER)) {
+                UserModel userModel = session.users().getUserById(realm,
+                        search.substring(SEARCH_ID_PARAMETER.length()).trim());
+                return new CountRepresentation(userModel != null && userPermissionEvaluator.canView(userModel) ? 1 : 0);
+            }
+
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put(UserModel.SEARCH, search.trim());
+
+            if (enabled != null) {
+                parameters.put(UserModel.ENABLED, enabled.toString());
+            }
+            if (emailVerified != null) {
+                parameters.put(UserModel.EMAIL_VERIFIED, emailVerified.toString());
+            }
+
+            if (userPermissionEvaluator.canView()) {
+                return new CountRepresentation(session.users().getUsersCount(realm, parameters));
+            } else {
+                if (Profile.isFeatureEnabled(Profile.Feature.ADMIN_FINE_GRAINED_AUTHZ)) {
+                    return new CountRepresentation(session.users().getUsersCount(realm, parameters,
+                            auth.groups().getGroupIdsWithViewPermission()));
+                } else {
+                    return new CountRepresentation(session.users().getUsersCount(realm, parameters));
+                }
+            }
+        } else if (last != null || first != null || email != null || username != null || emailVerified != null
+                || enabled != null || !searchAttributes.isEmpty()) {
+            Map<String, String> parameters = new HashMap<>();
+            if (last != null) {
+                parameters.put(UserModel.LAST_NAME, last);
+            }
+            if (first != null) {
+                parameters.put(UserModel.FIRST_NAME, first);
+            }
+            if (email != null) {
+                parameters.put(UserModel.EMAIL, email);
+            }
+            if (username != null) {
+                parameters.put(UserModel.USERNAME, username);
+            }
+            if (emailVerified != null) {
+                parameters.put(UserModel.EMAIL_VERIFIED, emailVerified.toString());
+            }
+            if (idpAlias != null) {
+                parameters.put(UserModel.IDP_ALIAS, idpAlias);
+            }
+            if (idpUserId != null) {
+                parameters.put(UserModel.IDP_USER_ID, idpUserId);
+            }
+            if (enabled != null) {
+                parameters.put(UserModel.ENABLED, enabled.toString());
+            }
+            if (exact != null) {
+                parameters.put(UserModel.EXACT, exact.toString());
+            }
+            parameters.putAll(searchAttributes);
+
+            if (userPermissionEvaluator.canView()) {
+                return new CountRepresentation(session.users().getUsersCount(realm, parameters));
+            } else {
+                if (Profile.isFeatureEnabled(Profile.Feature.ADMIN_FINE_GRAINED_AUTHZ)) {
+                    return new CountRepresentation(session.users().getUsersCount(realm, parameters,
+                            auth.groups().getGroupIdsWithViewPermission()));
+                } else {
+                    return new CountRepresentation(session.users().getUsersCount(realm, parameters));
+                }
+            }
+        } else if (userPermissionEvaluator.canView()) {
+            return new CountRepresentation(session.users().getUsersCount(realm));
+        } else {
+            if (Profile.isFeatureEnabled(Profile.Feature.ADMIN_FINE_GRAINED_AUTHZ)) {
+                return new CountRepresentation(
+                        session.users().getUsersCount(realm, auth.groups().getGroupIdsWithViewPermission()));
+            } else {
+                return new CountRepresentation(session.users().getUsersCount(realm));
+            }
+        }
     }
 }
