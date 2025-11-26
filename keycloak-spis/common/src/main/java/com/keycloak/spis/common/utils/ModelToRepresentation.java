42c0ca2e7b64bc714cbea8fb03267c54955817a1
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-11-26 18:07:47 +0200

    add get users endpoint

--- keycloak-spis/common/src/main/java/com/keycloak/spis/common/utils/ModelToRepresentation.java
+++ keycloak-spis/common/src/main/java/com/keycloak/spis/common/utils/ModelToRepresentation.java
@@ -6,6 +6,8 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 
 import com.keycloak.spis.common.GroupRealmRoles;
+import com.keycloak.spis.common.PlatformRealmRoles;
+import com.keycloak.spis.common.models.ExtendedUserRepresentation;
 import com.keycloak.spis.common.models.GroupRolesRepresentation;
 import com.keycloak.spis.common.models.GroupUserRepresentation;
 
@@ -17,6 +19,15 @@ public class ModelToRepresentation extends org.keycloak.models.utils.ModelToRepr
         groupUserRep.setRole(GroupRealmRoles.valueOf(group.getName()));
 
         return groupUserRep;
+    }
+
+    public static ExtendedUserRepresentation toRepresentation(KeycloakSession session, RealmModel realm,
+            UserModel user, PlatformRealmRoles role) {
+        ExtendedUserRepresentation userRep = new ExtendedUserRepresentation();
+        toRepresentation(session, realm, user, userRep, true);
+        userRep.setRole(role);
+
+        return userRep;
     }
 
     public static GroupRolesRepresentation toRepresentation(GroupModel group) {
--- keycloak-spis/spi-resources/src/main/java/com/keycloak/spis/resources/admin/resources/UsersAdminResource.java
+++ keycloak-spis/spi-resources/src/main/java/com/keycloak/spis/resources/admin/resources/UsersAdminResource.java
@@ -1,17 +1,41 @@
 package com.keycloak.spis.resources.admin.resources;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.resteasy.annotations.cache.NoCache;
+import org.keycloak.models.Constants;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.UserSessionModel;
 import org.keycloak.models.light.LightweightUserAdapter;
+import org.keycloak.services.resources.KeycloakOpenAPI;
 import org.keycloak.services.resources.admin.AdminEventBuilder;
 import org.keycloak.services.resources.admin.fgap.AdminPermissionEvaluator;
+import org.keycloak.services.resources.admin.fgap.UserPermissionEvaluator;
+
+import com.keycloak.spis.common.GroupRealmRoles;
+import com.keycloak.spis.common.PlatformRealmRoles;
+import com.keycloak.spis.common.models.ExtendedUserRepresentation;
+import com.keycloak.spis.common.utils.EnumUtils;
+import com.keycloak.spis.common.utils.ModelToRepresentation;
 
 import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 public class UsersAdminResource {
     private final AdminPermissionEvaluator auth;
@@ -49,5 +73,50 @@ public class UsersAdminResource {
         }
 
         return new UserAdminResource(session, user, auth, adminEvent);
+    }
+
+    @GET
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    @Tag(name = KeycloakOpenAPI.Admin.Tags.USERS)
+    @Operation(summary = "Get users Returns a stream of users, filtered according to query parameters.")
+    public Stream<ExtendedUserRepresentation> getUsers(
+            @Parameter(description = "A String contained in username, first or last name, or email. Default search behavior is prefix-based (e.g., foo or foo*). Use *foo* for infix search and \"foo\" for exact search.") @QueryParam("search") String search,
+            @Parameter(description = "Pagination offset") @QueryParam("first") Integer firstResult,
+            @Parameter(description = "Maximum results size (defaults to 100)") @QueryParam("max") Integer maxResults,
+            @Parameter(description = "Boolean representing if user is enabled or not") @QueryParam("enabled") Boolean enabled) {
+        UserPermissionEvaluator userPermissionEvaluator = auth.users();
+
+        userPermissionEvaluator.requireQuery();
+
+        firstResult = firstResult != null ? firstResult : -1;
+        maxResults = maxResults != null ? maxResults : Constants.DEFAULT_MAX_RESULTS;
+
+        Stream<UserModel> userModels = Stream.empty();
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put(UserModel.INCLUDE_SERVICE_ACCOUNT, "false");
+
+        if (Objects.nonNull(search)) {
+            attributes.put(UserModel.SEARCH, search.trim());
+        }
+
+        if (Objects.nonNull(enabled)) {
+            attributes.put(UserModel.ENABLED, enabled.toString());
+        }
+
+        userModels = session.users().searchForUserStream(realm, attributes, firstResult, maxResults);
+
+        return userModels.map(u -> {
+            System.out.println(u.getUsername());
+            RoleModel roleModel = u.getRealmRoleMappingsStream()
+                    .filter(r -> EnumUtils.isValidEnumValue(PlatformRealmRoles.class, r.getName()))
+                    .findFirst().orElseThrow(() -> new IllegalArgumentException("User " + u.getId() + " has no role"));
+
+            PlatformRealmRoles platformRole = Arrays.stream(PlatformRealmRoles.values())
+                    .filter(pr -> pr.name().equalsIgnoreCase(roleModel.getName()))
+                    .findFirst().orElse(null);
+
+            return ModelToRepresentation.toRepresentation(session, realm, u, platformRole);
+        });
     }
 }
